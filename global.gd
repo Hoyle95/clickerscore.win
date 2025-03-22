cc1328087906711728ad4b9c3887d946b8ed3e89
Hoyle95 <140357015+Hoyle95@users.noreply.github.com>
2025-03-22 00:11:24 +0000

    leaderboard works!

--- global.gd
+++ global.gd
@@ -1,12 +1,15 @@
 extends Node
 
 func _ready() -> void:
+	#print(sw_result)
 	SilentWolf.configure({
 		"api_key": "TWSUcPXlCx4HA8uYodIx04iApxPb15cC4IiLOgjQ",
 		"game_id": "ClickerScore",
-		"log_level": 0
+		"log_level": 1
 	})
 
 	SilentWolf.configure_scores({
-		"open_scene_on_close": "res://scenes/MainPage.tscn"
+		"open_scene_on_close": "res://scenes/mainMenu.tscn"
 	})
+	
+	var sw_result: Dictionary = await SilentWolf.Scores.get_scores().sw_get_scores_complete
--- scenes/leaderboard.gd
+++ scenes/leaderboard.gd
@@ -1,8 +1,18 @@
 extends Control
 
-func _ready():
-	var sw_result: Dictionary = await SilentWolf.Scores.get_scores(0).sw_get_scores_complete
-	print(sw_result)
+func _ready() -> void:
+	loadScores()
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainMenu.tscn")
+
+func loadScores() -> void:
+	if SilentWolf.Scores.scores == []:
+		var l = Label.new()
+		l.text = "Please Reload"
+		$VBoxContainer.add_child(l)
+	else:
+		for i in SilentWolf.Scores.scores:
+			var l = Label.new()
+			l.text = i.player_name + " - " + str(i.score)
+			$VBoxContainer.add_child(l)
