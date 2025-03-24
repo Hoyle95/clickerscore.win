f032794e3d244a52da28eb70a8672059938b31bd
Hoyle95 <140357015+Hoyle95@users.noreply.github.com>
2025-03-24 02:51:54 +0000

    slight visual improvements

--- godot-clicker-score/scenes/leaderboard.gd
+++ godot-clicker-score/scenes/leaderboard.gd
@@ -8,13 +8,19 @@ func _on_back_pressed() -> void:
 
 func loadScores() -> void:
 	if SilentWolf.Scores.scores == []:
+		$reload.visible = true
 		var l = Label.new()
 		l.text = "Please Reload"
+		l.set("theme_override_colors/font_color", Vector3(0,0,0))
+		l.align = 1
 		$VBoxContainer.add_child(l)
 	else:
 		for i in SilentWolf.Scores.scores:
 			var l = Label.new()
-			l.text = i.player_name + " - " + str(i.score)
+			l.text = i.player_name + " - " + str(int(i.score))
 			l.set("theme_override_colors/font_color", Vector3(0,0,0))
 			l.align = 1
 			$VBoxContainer.add_child(l)
+
+func _on_reload_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/leaderboard.tscn")
